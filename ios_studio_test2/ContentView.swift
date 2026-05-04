e76079e2677dd9ca299a3527306d78a69eb29723
Nason <jokerslade76@gmail.com>
2026-05-04 16:45:09 +1000

    test changes

--- ios_studio_test2/ContentView.swift
+++ ios_studio_test2/ContentView.swift
@@ -13,6 +13,12 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
             Text("Hello, world!")
         }
         .padding()
