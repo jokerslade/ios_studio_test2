1f74d9ee348a632eb8cb0076bd743fcbc0791fb2
Nason <jokerslade76@gmail.com>
2026-05-04 16:38:08 +1000

    Initial Commit

---
+++ ios_studio_test2/ios_studio_test2App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_studio_test2App.swift
+//  ios_studio_test2
+//
+//  Created by Nason on 2026/5/4.
+//
+
+import SwiftUI
+
+@main
+struct ios_studio_test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
